6cbb1683da74cfde3fd792bcd1978dfcc6d4d803
Benedikt <walser@pwners.de>
2020-04-14 20:36:22 +0200

    don't connect if already connecting or connected

--- ScClient/Socket.cs
+++ ScClient/Socket.cs
@@ -215,6 +215,7 @@ namespace ScClient
 
         public void Connect()
         {
+            if (_socket.State == WebSocketState.Connecting || _socket.State == WebSocketState.Open) return;
             _socket.Open();
         }
 
